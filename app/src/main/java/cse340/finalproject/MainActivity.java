055434d9c9971426862af5ffffd0618067a764be
Akilesh Kumar Konda Radhakrishnan <akonda@cs.washington.edu>
2023-05-31 18:37:52 -0700

    added 2 screens

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -2,25 +2,50 @@ package cse340.finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
     public static final FrameLayout.LayoutParams PARAMS = new FrameLayout.LayoutParams(
             FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
+    private SensorManager sensorManager;
+    private Sensor linearAccelerationSensor;
+    private TextView accelerationTextView;
+    private TextView resultantAccelerationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        accelerationTextView = findViewById(R.id.accelerationTextView);
+
+        // Initialize the sensor manager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // Check if the linear acceleration sensor is available
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+            // Get a reference to the linear acceleration sensor
+            linearAccelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        } else {
+            // Linear acceleration sensor is not available on this device
+            accelerationTextView.setText("Linear acceleration sensor not available");
+        }
 
 
         Button leftButton = findViewById(R.id.left);
@@ -49,40 +74,46 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the sensor listener
+        if (linearAccelerationSensor != null) {
+            sensorManager.registerListener(this, linearAccelerationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the sensor listener to save battery
+        if (linearAccelerationSensor != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            // Get the acceleration values
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
 
+            // Update the TextView with the acceleration values
+            String accelerationText = "X: " + x + "\nY: " + y + "\nZ: " + z;
+            accelerationTextView.setText(accelerationText);
+        }
+    }
 
-//        ViewGroup contents = findViewById(R.id.tab_contents); // Our primary view container
-//        TabView nav = findViewById(R.id.bottom_nav); // Tabs at the bottom of screen.
-//        contents.setBackgroundColor(Color.WHITE);
-//
-//        // Get the image margin from resources, in pixels (converted from dp)
-//        int imageMargin = getResources().getDimensionPixelSize(R.dimen.image_margin);
-//
-//        // Register callback on item selected. This is called each time a tab is pressed.
-//        nav.setOnNavigationItemSelectedListener(item -> {
-//            contents.removeAllViews(); // Clear the currently displayed view.
-//
-//            // Set tab contents based on selected item.
-//            switch (item.getItemId()) {
-//                case R.id.screen1:
-//                    contents.addView(getLayoutInflater().inflate(R.layout.activity_main, null), PARAMS);
-//                    return true;
-//                case R.id.screen2:
-//                    contents.addView(getLayoutInflater().inflate(R.layout.screen2, null), PARAMS);
-//                    return true;
-//                case R.id.screen3:
-//                    contents.addView(getLayoutInflater().inflate(R.layout.screen2, null), PARAMS);
-//                    return true;
-//                default:
-//                    Log.e("CSE340","Unrecognized nav item selected: " + item.getTitle());
-//            }
-//            return false;
-//        });
-//
-//        nav.setSelectedItemId(R.id.screen1);
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not implemented in this assignment.
+    }
 
+    private double resultantAcceleration(float x, float y, float z) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
     }
 }
--- app/src/main/java/cse340/finalproject/TimerActivity.java
+++ app/src/main/java/cse340/finalproject/TimerActivity.java
@@ -1,18 +1,123 @@
 package cse340.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class TimerActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity implements SensorEventListener {
+
+    public long timeMs;
+
+    private SensorManager sensorManager;
+    private Sensor linearAccelerationSensor;
+    private TextView accelerationTextView;
+
+    private double sumAcceleration;
+    private int accelerationCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen2);
+
+        accelerationTextView = findViewById(R.id.accDetails);
+
+        // Initialize the sensor manager
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        // Check if the linear acceleration sensor is available
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+            // Get a reference to the linear acceleration sensor
+            linearAccelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        } else {
+            // Linear acceleration sensor is not available on this device
+            accelerationTextView.setText("Linear acceleration sensor not available");
+        }
+
+        Button timerButton = findViewById(R.id.timerButton);
+        timerButton.setOnClickListener(new View.OnClickListener() {
+            private boolean isTimerRunning = false;
+            private long startTime;
+            private long elapsedTime;
+
+
+
+            @Override
+            public void onClick(View v) {
+                if (!isTimerRunning) {
+                    // Start the timer
+                    isTimerRunning = true;
+                    startTime = System.currentTimeMillis();
+                    timerButton.setText("Stop Timer"); // Update button text
+
+                    sumAcceleration = 0;
+                    accelerationCount = 0;
+
+                    // Create a new thread to update the timer display
+                    Thread timerThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (isTimerRunning) {
+                                // Calculate elapsed time
+                                elapsedTime = System.currentTimeMillis() - startTime;
+
+                                double averageAcceleration = sumAcceleration/accelerationCount;
+                                double averageVelocity = 0.5 * averageAcceleration * elapsedTime/1000.0;
+                                double distanceRuninMiles = 0.5 * averageAcceleration *
+                                        Math.pow(elapsedTime/1000.0, 2) * 0.000621371;
+
+                                // Update the timer display on the UI thread
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        // Update the timer display (assuming you have a TextView with id timerTextView)
+                                        TextView timerTextView = findViewById(R.id.timerTextView);
+                                        System.out.println("elapsed timekldsnfkjdskjad sf: " + elapsedTime);
+                                        timeMs = elapsedTime;
+                                        timerTextView.setText(formatTime(elapsedTime));
+
+                                        TextView accelerationTextView = findViewById(R.id.accDetails);
+                                        accelerationTextView.setText("Average Acceleration: " +
+                                                String.format("%.4f", averageAcceleration) + " metre/second^2");
+
+                                        TextView averageVelocityTextView = findViewById(R.id.avgVelTxt);
+                                        averageVelocityTextView.setText("Average Velocity: " +
+                                                String.format("%.4f", averageVelocity) +
+                                                " metre/second");
+
+                                        TextView distanceTextView = findViewById(R.id.totalDistanceTxt);
+                                        distanceTextView.setText("Distance Equivalent: " +
+                                                String.format("%.4f", distanceRuninMiles) +
+                                                " miles");
+
+                                    }
+                                });
+
+                                try {
+                                    Thread.sleep(10); // Delay between updates (adjust as needed)
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    });
+                    timerThread.start();
+                } else {
+                    // Stop the timer
+                    isTimerRunning = false;
+                    timerButton.setText("Start Timer"); // Update button text
+                }
+            }
+        });
 
         Button leftButton = findViewById(R.id.left);
         leftButton.setOnClickListener(new View.OnClickListener() {
@@ -40,5 +145,59 @@ public class TimerActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the sensor listener
+        if (linearAccelerationSensor != null) {
+            sensorManager.registerListener(this, linearAccelerationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the sensor listener to save battery
+        if (linearAccelerationSensor != null) {
+            sensorManager.unregisterListener(this);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION) {
+            // Get the acceleration values
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            double resultantAcc = resultantAcceleration(x, y, z);
+
+            // Update the TextView with the acceleration values
+//            String accelerationText = "X: " + x + "\nY: " + y + "\nZ: " + z;
+//            accelerationTextView.setText(accelerationText);
+            sumAcceleration += resultantAcc;
+            accelerationCount++;
+        }
+    }
+
+    private double resultantAcceleration(float x, float y, float z) {
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Not implemented in this assignment.
+    }
+
+    // Helper method to format the elapsed time as HH:MM:SS
+    private String formatTime(long elapsedTime) {
+        long seconds = elapsedTime / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes % 60, seconds % 60);
     }
 }
